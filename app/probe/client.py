3acbbd94c3b8dc3d143b3b3a56f9581db8da3d1b
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2013-06-07 14:48:22 +0200

    Added base classes

---
+++ app/probe/client.py
@@ -0,0 +1,18 @@
+'''
+Created on 7 juin 2013
+
+@author: francois
+'''
+import threading;
+
+class Client(threading.Thread):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ app/probe/main.py
@@ -0,0 +1,8 @@
+'''
+Created on 7 juin 2013
+
+@author: francois
+'''
+
+if __name__ == '__main__':
+    pass
