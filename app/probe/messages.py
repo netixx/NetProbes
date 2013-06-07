64ac5f49fc5853e39349d8adacf89bb67d6893ef
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2013-06-07 15:31:30 +0200

    Added basic skeletton for messages

---
+++ app/probe/messages.py
@@ -0,0 +1,125 @@
+'''
+Created on 7 juin 2013
+
+@author: francois
+'''
+
+class Message(object):
+    '''
+    classdocs
+    Message sent from one probe to another
+    '''
+
+
+class Add(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+class Transfer(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+class Hello(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+class Do(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        
+class Bye(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+class Prepare(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    
+    
+class Ready(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+class Abort(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        
+class Over(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+class Result(Message):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
