191195b79bca9b8402300fb747486f8033959e25
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2013-06-07 14:25:22 +0200

    Added class for Command line Interface

---
+++ app/commander/cli.py
@@ -0,0 +1,17 @@
+'''
+Created on 7 juin 2013
+
+@author: francois
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
