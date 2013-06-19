2144d5e8a1c10793a482060a716713ae60d7277c
Gaspard FEREY <gaspard.ferey@hotmail.fr>
2013-06-19 15:10:33 +0200

    Create empty test

---
+++ app/probe/tests/emptyTest.py
@@ -0,0 +1,15 @@
+'''
+Created on 13 juin 2013
+
+@author: francois
+'''
+from probe.test import Test
+
+class emptyTest(Test):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        pass
