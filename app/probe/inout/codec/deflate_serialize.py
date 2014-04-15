14fb7f71967f5d53613e30da3c0d2fb044fc5c9e
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-04-15 14:25:19 +0200

    New Codec, serialisation + deflate

---
+++ app/probe/inout/codec/deflate_serialize.py
@@ -0,0 +1,20 @@
+'''
+Created on 15 avr. 2014
+
+@author: francois
+'''
+import pickle
+import zlib
+_ENCODING = 'latin1'
+_LEVEL = 6
+
+'''Encode message to send on the network by serialization'''
+def encode(message):
+    o = pickle.dumps(message, 3)
+    r = zlib.compress(o, _LEVEL)
+    return r
+
+
+'''Decode message received with serialisation'''
+def decode(message):
+    return pickle.loads(zlib.decompress(message))
