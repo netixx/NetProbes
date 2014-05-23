41798e2fc9940fb814c39fbce5ced5fc45b441ad
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-05-23 11:52:45 +0200

    Use exec to propagate signal to python in start script

--- start.sh
+++ start.sh
@@ -7,7 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pymain=$DIR"/app/probe/main.py"
 py=$(which python3)
 
-#TODO: read id from cmdline
-#TODO: read commander from command line
-$py $pymain "$@" 
+exec $py $pymain "$@"
 
